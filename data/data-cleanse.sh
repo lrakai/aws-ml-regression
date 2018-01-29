6ba7c3f9abfd183fda8ebf6e920bca9f4deae000
Logan Rakai <lmrakai@gmail.com>
2018-01-29 14:55:43 -0700

    strip ending commas in data

--- data/data-cleanse.sh
+++ data/data-cleanse.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# aggregate raw monthly data into a single file (5671365 records)
+# aggregate raw monthly data into a single file with trailing commas removed (5671365 records)
 # remove flights that never arrived (canceled, etc.) as indicated by the presence of ,, in a raw CSV (5573747 records) 
 # zip the file
 
@@ -9,12 +9,14 @@ name=Flights_Dec2016-Nov2017
 aggregate_file=$name.csv
 zip_file=$name.zip
 
-header=`head -1 $(ls $raw_data_dir/*\.csv | head -1)`
+header=`head -1 $(ls $raw_data_dir/*\.csv | head -1) | sed -i 's/,$//g'`
 
 echo $header > $aggregate_file
 for file in `ls $raw_data_dir/*\.csv`; do
-    tail --lines=+2 $file | grep -v ,, >> $aggregate_file 
+    echo "Processing $file"
+    tail --lines=+2 $file | grep -v ,, | sed -i 's/,$//g' >> $aggregate_file 
 done
 
 zip $zip_file $aggregate_file
-rm $aggregate_file
+
+echo "Finished"
